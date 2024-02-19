3a9d8ad80742a7ba89ad27e26498f99b3d16ee19
Moniaar <WWW.omniaahmed1562001@gmail.com>
2024-02-19 07:16:29 +0200

    #10 replaced the if statement condition by another line

--- models/user.py
+++ models/user.py
@@ -6,16 +6,21 @@ from models import storage_type
 
 
 class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
+    """This class defines a user by various attributes
+       __tablename__: users mapping table
+        email: sqlalchemy String column: the user's entered email
+        password: sqlalchemy String column: the user's entered password
+        first_name: sqlalchemy String column: the user's firstname
+        last_name: sqlalchemy String column: the user's lastname"""
     __tablename__ = 'users'
+
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
 
     if storage_type != 'db':
         email = ''
         password = ''
         first_name = ''
         last_name = ''
-    else:
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
