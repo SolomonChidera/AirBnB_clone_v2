87f5d955a9bb78359aea49053939d910872e44aa
SolomonChidera <solochidera10@gmail.com>
2024-02-20 18:51:08 +0100

    Handle the user, review, state models

--- models/user.py
+++ models/user.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
+from models.place import Place
+from models.review import Review
 from sqlalchemy import Column, String
 from models import storage_type
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes
-       __tablename__: users mapping table
-        email: sqlalchemy String column: the user's entered email
-        password: sqlalchemy String column: the user's entered password
+    class Attributes:
+        __tablename__: mapping table
+        email: sqlalchemy String column: the user's email
+        password: sqlalchemy String column: the user's password
         first_name: sqlalchemy String column: the user's firstname
-        last_name: sqlalchemy String column: the user's lastname"""
+        last_name: sqlalchemy String column: the user's lastname
+    """
     __tablename__ = 'users'
 
     if storage_type != 'db':
@@ -24,3 +29,7 @@ class User(BaseModel, Base):
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
+        places = relationship('Place', backref='user',
+                              cascade='all, delete')
+        reviews = relationship('Review', backref='user',
+                               cascade='all, delete')
